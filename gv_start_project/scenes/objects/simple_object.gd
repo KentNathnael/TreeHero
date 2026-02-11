c6237bdcd8db7f98f8a94cfba6eb23db1c2fa11d
QaiCenat <m.qaishar.arif.navarino@gmail.com>
2026-02-12 00:43:59 +0700

    adding more detail on map and fixing camera movement

--- gv_start_project/scenes/objects/simple_object.gd
+++ gv_start_project/scenes/objects/simple_object.gd
@@ -1,71 +1,71 @@
-#@tool
-#extends StaticBody2D
-#
-#@export_range(0,3,1) var size: int:
-	#set(value):
-		#size = value
-		#$Sprite2D.frame_coords = Vector2i(size, style)
-#@export_enum('Bush', 'Rock') var style: int:
-	#set(value):
-		#style = value
-		#$Sprite2D.frame_coords = Vector2i(size, style)
-#@export var random: bool
-#@export_tool_button('Randomize', "Callable") var randomizer = randomize
-#
-#
-#func _ready() -> void:
-	#if random:
-		#size = randi_range(0, $Sprite2D.hframes - 1)
-		#style = [0,1].pick_random()
-	#$Sprite2D.frame_coords = Vector2i(size, style)
-	#$CollisionShape2D.disabled = size < 2
-	#z_index = -1 if size < 2 else 0
-#
-##
-#func randomize():
-	#size = randi_range(0, $Sprite2D.hframes - 1)
-	#style = [0,1].pick_random()
-	#$Sprite2D.frame_coords = Vector2i(size, style)
-
-
 @tool
 extends StaticBody2D
 
-var _size: int = 0
-var _style: int = 0
-
-@export_range(0, 3, 1) var size: int:
-	get:
-		return _size
+@export_range(0,3,1) var size: int:
 	set(value):
-		_size = value
-		update_visual()
-
-@export_enum("Bush", "Rock") var style: int:
-	get:
-		return _style
+		size = value
+		$Sprite2D.frame_coords = Vector2i(size, style)
+@export_enum('Bush', 'Rock') var style: int:
 	set(value):
-		_style = value
-		update_visual()
+		style = value
+		#$Sprite2D.frame_coords = Vector2i(size, style)
+@export var random: bool
+@export_tool_button('Randomize', "Callable") var randomizer = randomize
 
-@export var random: bool = false
-@export_tool_button("Randomize") var randomizer = randomize
 
 func _ready() -> void:
 	if random:
-		randomize()
-	update_physics()
+		size = randi_range(0, $Sprite2D.hframes - 1)
+		style = [0,1].pick_random()
+	$Sprite2D.frame_coords = Vector2i(size, style)
+	$CollisionShape2D.disabled = size < 2
+	z_index = -1 if size < 2 else 0
 
-func update_visual():
-	if not is_inside_tree():
-		return
-	$Sprite2D.frame_coords = Vector2i(_size, _style)
-	update_physics()
-
-func update_physics():
-	$CollisionShape2D.disabled = _size < 2
-	z_index = -1 if _size < 2 else 0
-
+#
 func randomize():
 	size = randi_range(0, $Sprite2D.hframes - 1)
-	style = [0, 1].pick_random()
+	style = [0,1].pick_random()
+	$Sprite2D.frame_coords = Vector2i(size, style)
+
+
+#@tool
+#extends StaticBody2D
+#
+#var _size: int = 0
+#var _style: int = 0
+#
+#@export_range(0, 3, 1) var size: int:
+	#get:
+		#return _size
+	#set(value):
+		#_size = value
+		#update_visual()
+#
+#@export_enum("Bush", "Rock") var style: int:
+	#get:
+		#return _style
+	#set(value):
+		#_style = value
+		#update_visual()
+#
+#@export var random: bool = false
+#@export_tool_button("Randomize") var randomizer = randomize
+#
+#func _ready() -> void:
+	#if random:
+		#randomize()
+	#update_physics()
+#
+#func update_visual():
+	#if not is_inside_tree():
+		#return
+	#$Sprite2D.frame_coords = Vector2i(_size, _style)
+	#update_physics()
+#
+#func update_physics():
+	#$CollisionShape2D.disabled = _size < 2
+	#z_index = -1 if _size < 2 else 0
+#
+#func randomize():
+	#size = randi_range(0, $Sprite2D.hframes - 1)
+	#style = [0, 1].pick_random()
