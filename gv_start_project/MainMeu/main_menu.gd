9531b72149e6c8a4a5fa6226278c80f6645fa25d
Kent Nathanael <100405144+KentNathnael@users.noreply.github.com>
2026-02-11 22:28:30 +0700

    added Main menu

---
+++ gv_start_project/MainMeu/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+var button_type = null
+
+func _on_start_but_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
+
+
+func _on_option_pressed() -> void:
+	print('option')
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
