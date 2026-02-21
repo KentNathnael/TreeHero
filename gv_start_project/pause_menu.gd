4a85662faa576fba2ee4c1a44ef0b19063d312fa
WilsonNgode <wilsonhandojo@student.ub.ac.id>
2026-02-21 10:27:59 +0700

    pause button worked

--- gv_start_project/pause_menu.gd
+++ gv_start_project/pause_menu.gd
@@ -4,11 +4,28 @@ func _ready():
 	visible = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().paused = !get_tree().paused
-		visible = get_tree().paused
+#func _input(event):
+	##print("masuk pause")
+	#if event.is_action_just_pressed("ui_cancel"):
+		#if get_tree().paused:
+			#visible = false
+			#get_tree().paused = false
+		#else:
+			#visible = true
+			#get_tree().paused = true
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		if get_tree().paused:
+			#print("UNPAUSE")
+			visible = false
+			get_tree().paused = false
+		else:
+			print("PAUSE")
+			visible = true
+			get_tree().paused = true
 
 func _on_button_pressed():
-	get_tree().paused = false
+	#print("RESUME DIKLIK")
 	visible = false
+	get_tree().paused = false
